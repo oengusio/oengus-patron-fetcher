381c81e22dcdb6e7de2d75ce0488124e161acea5
duncte123 <contact@duncte123.me>
2020-12-21 09:44:56 +0100

    Write simple base for fetching refresh token

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	patrons, _ := FetchPatrons()
+	/*patrons, _ := FetchPatrons()
 
 	fmt.Println("There are", len(patrons.Data), "patrons")
 
 	// for {key}, {value} := range {list}
 	for _, patron := range patrons.Data {
 		fmt.Println("Patron", patron.Attributes.FullName)
-	}
+	}*/
+
+	mux := http.NewServeMux()
+
+	// Using /pagetier here becuase "/" would catch all routes sent to it
+	mux.HandleFunc("/pagetier", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		w.Write([]byte("{\"patrons\": []}"))
+	})
+
+	log.Fatal(http.ListenAndServe(":9000", mux))
 }
--- patreon.go
+++ patreon.go
@@ -12,6 +12,15 @@ import (
 var campaignId = os.Getenv("PATREON_CAMPAIGN_ID")
 var httpClient = http.Client{}
 
+
+/// <editor-fold name="Refresh token response">
+type PatreonTokens struct {
+	AccessToken string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+/// </editor-fold>
+
+/// <editor-fold name="Patrons response">
 type PatreonMembersResponse struct {
 	Data []PatreonMembersData `json:"data"`
 }
@@ -25,16 +34,13 @@ type PatreonMembersAttribute struct {
 	PatronStatus string `json:"patron_status"`
 	WillPayAmountCents int `json:"will_pay_amount_cents"`
 }
+/// </editor-fold>
 
-func RefreshToken() {
-	//
-}
+func RefreshToken(tokens PatreonTokens) (PatreonTokens, error) {
+	// TODO: test if initial refresh token stays the same
+	var response PatreonTokens
 
-// TODO: https://www.reddit.com/r/golang/comments/2xmnvs/returning_nil_for_a_struct/
-func FetchPatrons() (PatreonMembersResponse, error) {
-	var response PatreonMembersResponse
-
-	url := fmt.Sprintf("https://www.patreon.com/api/oauth2/v2/campaigns/%s/members", campaignId)
+	url := "https://www.patreon.com/api/oauth2/token"
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -42,11 +48,16 @@ func FetchPatrons() (PatreonMembersResponse, error) {
 	}
 
 	req.Header.Set("User-Agent", "oengus.io/patreon-fetcher")
-	req.Header.Set("Authorisation", "Bearer TODO")
 
-	res, getErr := httpClient.Do(req)
-	if getErr != nil {
-		log.Fatal(getErr)
+	query := req.URL.Query()
+	query.Set("grant_type", "refresh_token")
+	query.Set("refresh_token", tokens.RefreshToken)
+	query.Set("client_id", os.Getenv("PATREON_CLIENT_ID"))
+	query.Set("client_secret", os.Getenv("PATREON_CLIENT_SECRET"))
+
+	res, httpErr := httpClient.Do(req)
+	if httpErr != nil {
+		log.Fatal(httpErr)
 	}
 
 	if res.Body != nil {
@@ -58,10 +69,52 @@ func FetchPatrons() (PatreonMembersResponse, error) {
 		log.Fatal(readErr)
 	}
 
-	err2 := json.Unmarshal(body, &response)
-	if err2 != nil {
-		fmt.Println(err2)
-		return response, err2
+	jsonErr := json.Unmarshal(body, &response)
+	if jsonErr != nil {
+		fmt.Println(jsonErr)
+		return response, jsonErr
+	}
+
+	return response, nil
+}
+
+// TODO: https://www.reddit.com/r/golang/comments/2xmnvs/returning_nil_for_a_struct/
+func FetchPatrons(tokens PatreonTokens) (PatreonMembersResponse, error) {
+	var response PatreonMembersResponse
+
+	url := fmt.Sprintf("https://www.patreon.com/api/oauth2/v2/campaigns/%s/members", campaignId)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+		return response, err
+	}
+
+	req.Header.Set("User-Agent", "oengus.io/patreon-fetcher")
+	req.Header.Set("Authorisation", "Bearer " + tokens.AccessToken)
+
+	query := req.URL.Query()
+	query.Set("fields%5Bmember%5D", "full_name,patron_status,will_pay_amount_cents")
+	query.Set("&page%5Bcount%5D", "1000")
+
+	res, httpErr := httpClient.Do(req)
+	if httpErr != nil {
+		log.Fatal(httpErr)
+	}
+
+	// defer calls are not executed until the function returns
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	jsonErr := json.Unmarshal(body, &response)
+	if jsonErr != nil {
+		fmt.Println(jsonErr)
+		return response, jsonErr
 	}
 
 	return response, nil
