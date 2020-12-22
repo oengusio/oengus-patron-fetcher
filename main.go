c5678039e327a320efc99181807db29da1b435b3
duncte123 <contact@duncte123.me>
2020-12-22 19:30:09 +0100

    Add cors headers

--- main.go
+++ main.go
@@ -109,6 +109,27 @@ func InitApp() {
     StartUpdatePatronTimer()
 }
 
+func logRequestHandler(h http.Handler) http.Handler {
+    fn := func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+        w.Header().Set("Access-Control-Allow-Headers",
+            "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+        // Stop here if its Preflighted OPTIONS request
+        if r.Method == "OPTIONS" {
+            return
+        }
+
+        // call the original http.Handler we're wrapping
+        h.ServeHTTP(w, r)
+    }
+
+    // http.HandlerFunc wraps a function so that it
+    // implements http.Handler interface
+    return http.HandlerFunc(fn)
+}
+
 func main() {
     InitApp()
 
@@ -140,8 +161,18 @@ func main() {
 }`))
     })
 
+    var handler http.Handler = mux
+    // wrap mux with our logger. this will
+    handler = logRequestHandler(handler)
+
+    server := &http.Server{
+        Addr: ":9000",
+        ReadTimeout:  120 * time.Second,
+        WriteTimeout: 120 * time.Second,
+        IdleTimeout:  120 * time.Second, // introduced in Go 1.8
+        Handler:      handler,
+    }
 
     log.Println("Listening to port 9000")
-
-    log.Fatal(http.ListenAndServe(":9000", mux))
+    log.Fatal(server.ListenAndServe())
 }
