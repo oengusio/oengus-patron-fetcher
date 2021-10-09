de7ed90784fc997bae21244d1bb1b4fdf77b3116
duncte123 <contact@duncte123.me>
2021-10-09 17:41:08 +0200

    Allow all headers

--- main.go
+++ main.go
@@ -32,8 +32,7 @@ func logRequestHandler(h http.Handler) http.Handler {
     fn := func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-        w.Header().Set("Access-Control-Allow-Headers",
-            "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+        w.Header().Set("Access-Control-Allow-Headers", "*")
 
         // Stop here if its Preflighted OPTIONS request
         if r.Method == "OPTIONS" {
