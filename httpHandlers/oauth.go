30f4fd9754c920bc6e1ea49f8d5a292dc2d2469f
duncte123 <contact@duncte123.me>
2022-01-26 19:14:32 +0100

    Allow for set origins to login

--- httpHandlers/oauth.go
+++ httpHandlers/oauth.go
@@ -1,46 +1,69 @@
 package httpHandlers
 
 import (
-    "encoding/json"
-    "net/http"
-    "oenugs-patreon/patreon"
+	"encoding/json"
+	"net/http"
+	"oenugs-patreon/patreon"
+	"os"
+	"strings"
 )
 
+// https://play.golang.org/p/Qg_uv_inCek
+// contains checks if a string is present in a slice
+func arrayContains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
 func OauthAuthorize(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-    if r.Method != http.MethodGet {
-        w.WriteHeader(http.StatusMethodNotAllowed)
-        w.Write([]byte(`{"error": "Method not allowed"}`))
-        return
-    }
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(`{"error": "Method not allowed"}`))
+		return
+	}
 
-    code := r.URL.Query().Get("code")
+	code := r.URL.Query().Get("code")
 
-    if code == "" {
-        w.WriteHeader(http.StatusBadRequest)
-        w.Write([]byte(`{"error": "missing code in request"}`))
-        return
-    }
+	if code == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "missing code in request"}`))
+		return
+	}
 
-    token, err := patreon.Oauth2FetchToken(code)
+	origin := r.Header.Get("Origin")
+	allowedOrigins := strings.Split(os.Getenv("OENGUS_BASE"), ",")
 
-    if err != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write([]byte(err.Error()))
-        return
-    }
+	if !arrayContains(allowedOrigins, origin) {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "Origin is not allowed"}`))
+		return
+	}
 
-    user, fetchErr := patreon.Oauth2FetchUser(token)
+	token, err := patreon.Oauth2FetchToken(code, origin)
 
-    if fetchErr != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write([]byte(fetchErr.Error()))
-        return
-    }
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
-    w.WriteHeader(http.StatusOK)
-    data,_ := json.Marshal(user)
+	user, fetchErr := patreon.Oauth2FetchUser(token)
 
-    w.Write(data)
+	if fetchErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fetchErr.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(user)
+
+	w.Write(data)
 }
--- patreon/oauth.go
+++ patreon/oauth.go
@@ -1,124 +1,124 @@
 package patreon
 
 import (
-    "bytes"
-    "encoding/json"
-    "errors"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "net/url"
-    "oenugs-patreon/structs"
-    "os"
-    "strings"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"oenugs-patreon/structs"
+	"os"
+	"strings"
 )
 
-func Oauth2FetchToken(code string) (structs.PatreonTokens, error) {
-    var response structs.PatreonTokens
+func Oauth2FetchToken(code string, origin string) (structs.PatreonTokens, error) {
+	var response structs.PatreonTokens
 
-    query := url.Values{}
-    query.Set("grant_type", "authorization_code")
-    query.Set("code", code)
-    query.Set("client_id", os.Getenv("PATREON_CLIENT_ID"))
-    query.Set("client_secret", os.Getenv("PATREON_CLIENT_SECRET"))
-    query.Set("redirect_uri", os.Getenv("OENGUS_BASE") + "/user/settings/sync/patreon")
-    queryBytes := []byte(query.Encode())
+	query := url.Values{}
+	query.Set("grant_type", "authorization_code")
+	query.Set("code", code)
+	query.Set("client_id", os.Getenv("PATREON_CLIENT_ID"))
+	query.Set("client_secret", os.Getenv("PATREON_CLIENT_SECRET"))
+	query.Set("redirect_uri", origin+"/user/settings/sync/patreon")
+	queryBytes := []byte(query.Encode())
 
-    apiUrl := "https://api.patreon.com/oauth2/token"
-    req, err := http.NewRequest(http.MethodPost, apiUrl, bytes.NewBuffer(queryBytes))
+	apiUrl := "https://api.patreon.com/oauth2/token"
+	req, err := http.NewRequest(http.MethodPost, apiUrl, bytes.NewBuffer(queryBytes))
 
-    if err != nil {
-        log.Println(err)
-        return response, err
-    }
+	if err != nil {
+		log.Println(err)
+		return response, err
+	}
 
-    req.Header.Set("User-Agent", "oengus.io/patreon-fetcher")
-    req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("User-Agent", "oengus.io/patreon-fetcher")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-    res, httpErr := httpClient.Do(req)
-    if httpErr != nil {
-        log.Println(httpErr)
-        return response, httpErr
-    }
+	res, httpErr := httpClient.Do(req)
+	if httpErr != nil {
+		log.Println(httpErr)
+		return response, httpErr
+	}
 
-    if res.Body != nil {
-        defer res.Body.Close()
-    }
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
 
-    body, readErr := ioutil.ReadAll(res.Body)
-    if readErr != nil {
-        log.Println(readErr)
-        return response, readErr
-    }
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Println(readErr)
+		return response, readErr
+	}
 
-    // In case I ever need the new tokens
-    strBody := string(body)
+	// In case I ever need the new tokens
+	strBody := string(body)
 
-    if strings.Contains(strBody, "error") {
-        return response, errors.New(strBody)
-    }
+	if strings.Contains(strBody, "error") {
+		return response, errors.New(strBody)
+	}
 
-    jsonErr := json.Unmarshal(body, &response)
-    if jsonErr != nil {
-        log.Println(jsonErr)
-        return response, jsonErr
-    }
+	jsonErr := json.Unmarshal(body, &response)
+	if jsonErr != nil {
+		log.Println(jsonErr)
+		return response, jsonErr
+	}
 
-    return response, nil
+	return response, nil
 }
 
 func Oauth2FetchUser(token structs.PatreonTokens) (structs.PatronRelationshipUser, error) {
-    var response structs.PatronRelationshipUser
+	var response structs.PatronRelationshipUser
 
-    apiUrl := "https://api.patreon.com/oauth2/v2/identity"
-    req, err := http.NewRequest(http.MethodGet, apiUrl, nil)
+	apiUrl := "https://api.patreon.com/oauth2/v2/identity"
+	req, err := http.NewRequest(http.MethodGet, apiUrl, nil)
 
-    if err != nil {
-        log.Println(err)
-        return response, err
-    }
+	if err != nil {
+		log.Println(err)
+		return response, err
+	}
 
-    query := req.URL.Query()
-    // we get info about our own campaign if we do not set the identity.memberships scope
-    query.Set("include", "memberships")
-    query.Set("fields[member]", "patron_status,will_pay_amount_cents")
+	query := req.URL.Query()
+	// we get info about our own campaign if we do not set the identity.memberships scope
+	query.Set("include", "memberships")
+	query.Set("fields[member]", "patron_status,will_pay_amount_cents")
 
-    req.URL.RawQuery = query.Encode()
+	req.URL.RawQuery = query.Encode()
 
-    req.Header.Set("User-Agent", "oengus.io/patreon-fetcher")
-    req.Header.Set("Authorization", fmt.Sprintf("%s %s", token.TokenType, token.AccessToken))
+	req.Header.Set("User-Agent", "oengus.io/patreon-fetcher")
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", token.TokenType, token.AccessToken))
 
-    res, httpErr := httpClient.Do(req)
-    if httpErr != nil {
-        log.Println(httpErr)
-        return response, httpErr
-    }
+	res, httpErr := httpClient.Do(req)
+	if httpErr != nil {
+		log.Println(httpErr)
+		return response, httpErr
+	}
 
-    if res.Body != nil {
-        defer res.Body.Close()
-    }
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
 
-    body, readErr := ioutil.ReadAll(res.Body)
-    if readErr != nil {
-        log.Println(readErr)
-        return response, readErr
-    }
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Println(readErr)
+		return response, readErr
+	}
 
-    // In case I ever need the new tokens
-    strBody := string(body)
+	// In case I ever need the new tokens
+	strBody := string(body)
 
-    log.Println(strBody)
+	log.Println(strBody)
 
-    if strings.Contains(strBody, "error") {
-        return response, errors.New(strBody)
-    }
+	if strings.Contains(strBody, "error") {
+		return response, errors.New(strBody)
+	}
 
-    jsonErr := json.Unmarshal(body, &response)
-    if jsonErr != nil {
-        log.Println(jsonErr)
-        return response, jsonErr
-    }
+	jsonErr := json.Unmarshal(body, &response)
+	if jsonErr != nil {
+		log.Println(jsonErr)
+		return response, jsonErr
+	}
 
-    return response, nil
+	return response, nil
 }
